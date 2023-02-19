4e4a37c21a87d203c4cfb563004dfded7c6c1c1f
Amina <Nnalfa@yahoo.com>
2023-02-19 11:36:44 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-/*code that prints a sentence*/
-int main(void){
+/**
+ * code that prints a sentence
+ */
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle\n");
 	return(0);
 }
