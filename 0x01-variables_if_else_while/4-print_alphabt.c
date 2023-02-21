852e9a9e5621344194d5f9d3f0b1749ba5bc7e45
Amina <Nnalfa@yahoo.com>
2023-02-21 13:46:31 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+		putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 this program assigns a random number to the value n
 this program assigns a random number to a variable
  a program that prints the alphabet in lowercase, followed by a new line
+a program that prints the alphabet in lowercase, followed by a new line
