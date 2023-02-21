ca8f8c8067bcad6a3cdcb62313ff3890a37e0441
Amina <Nnalfa@yahoo.com>
2023-02-21 14:27:34 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", 1);
+	}
+	putchar('\n');
+	return (0);
+}
