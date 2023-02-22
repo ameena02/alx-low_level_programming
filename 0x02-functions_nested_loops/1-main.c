5a51383c494f94b6673079aadd235dadec611b5c
Amina <Nnalfa@yahoo.com>
2023-02-21 16:30:54 -0800

    task 0

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,19 @@
+#include "school.s"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
