15b598f2c9a6a4b42fe63b6ef420ff3366209198
Amina <Nnalfa@yahoo.com>
2023-02-27 13:21:58 -0800

    correction

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-//*length of the password to be generated
+    char a[10],c[10];
+    int i,j,k=0;
+    printf("#Keygen by b44nz0r\n\n");
 
-	int N = 10;
-	randomPasswordGeneration(N);
+    while (k <5 || k >=10)
+    {
+          if (k !=0)
+             printf("\nThe username length should be 5 to 10 alphabets\n");
 
-	return (0);
+         printf("enter username: ");
+         scanf("%s",a);
+         k = strlen(a);
+    }
+
+    i = k-1;
+    j = 0;
+
+    while (i >= 0)
+    {
+          c[j] = a[i]+i;
+          i--;
+          j++;
+    }
+
+    c[j] = 0;
+    printf("\nThe password is %s\n",c);
+    printf("\nHit Enter to Exit\n");
+    getchar();
+    getchar();
 }
