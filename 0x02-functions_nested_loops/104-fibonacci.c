72c04e4cfaff9a733a5cff80566c2c6071c3b823
Amina <Nnalfa@yahoo.com>
2023-02-27 04:35:01 -0800

    correction

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 	int i;
-	long int fibonacci[50], sum = 2;
+	long int fibonacci[98], sum = 2;
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
 
-	for (i = 2; i < 50; i++)
+	for (i = 2; i <= 98; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		if ((fibonacci[i] % 2) == 0 && fibonacci[i] < 4000000)
