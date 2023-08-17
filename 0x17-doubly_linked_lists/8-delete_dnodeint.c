1d32eddcce7c5fd3569fef0b55d63c285d194a5d
Amina Alfa <Nnaalfa@yahoo.com>
2023-08-17 14:25:09 -0700

    Doubly LL done

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes node at the given position.
+ * @head: double pointer to list.
+ * @index: index of inserting position.
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int x = 0;
+	dlistint_t *bfr_node = *head, *tmp_node = *head;
+
+	if (index == 0 && *head)
+	{
+		if ((*head)->next)
+		{
+			*head = (*head)->next;
+			(*head)->prev = NULL;
+			free(tmp_node);
+		}
+		else
+			*head = NULL;
+		return (1);
+	}
+	while (x < index - 1 && bfr_node)
+	{
+		bfr_node = bfr_node->next;
+		x++;
+	}
+	if (bfr_node)
+	{
+		tmp_node = bfr_node->next;
+		if (tmp_node->next)
+			tmp_node->next->prev = bfr_node;
+		bfr_node->next = tmp_node->next;
+		free(tmp_node);
+		return (1);
+	}
+	return (-1);
+}
